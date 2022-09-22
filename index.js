ec1d91f07091ab80871e864747287a15a004e292
Adam Weaver <adamweaver90@gmail.com>
2022-09-21 21:09:28 -0400

    commit tree 4f98c6ca6dfae4a8475461d3d5c68ebcf49b83e6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(cat) {
+    return cats.push(cat)
+}
+
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat)
+}
+
+function destructivelyRemoveLastCat(cat) {
+    return cats.pop(cat)
+}
+
+function destructivelyRemoveFirstCat(cat) {
+    return cats.shift(cat)
+}
